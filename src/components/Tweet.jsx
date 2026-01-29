b1dd04e1bef3960b9e0d5e383f76c7db8de9a0b1
yashasveesingh078 <yashasveesingh078@gmail.com>
2026-01-29 19:59:03 +0530

    tweet updated again

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -28,13 +28,13 @@ const Tweet = ({ tweet }) => {
             />
           )}
 
-         {/* actions
+         {/* actions */}
          <div className="flex justify-between mt-2 text-neutral-500 text-[12px] max-w-[420px]">
              <span className="hover:text-blue-400">💬 289</span>
            <span className="hover:text-green-400">🔁 2.9K</span>
              <span className="hover:text-pink-500">❤️ 11K</span>
           <span>📊 255K</span>
-         </div> */}
+         </div>
        </div>
       </div>
     </div>
