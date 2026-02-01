901140fca36086a78b6a7eca41d520bf80c89c4d
yashasveesingh078 <yashasveesingh078@gmail.com>
2026-02-01 10:14:18 +0530

    something

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/X-clone/", // 👈 repo ka exact naam
+  base: "/", // 👈 repo ka exact naam
 });
